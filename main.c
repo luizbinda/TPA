aa591d3cf0d9d7e787e4d7b1c343e153e7864528
Luiz Fernand Binda Pimenta <49559951+luizbinda@users.noreply.github.com>
2019-08-19 18:35:35 -0300

    att

--- main.c
+++ main.c
@@ -5,10 +5,16 @@
 typedef struct dadosBancariosOriginal{
     int id;
     char nome[50];
-    char saldo[50];
+    float saldo;
     struct dadosBancariosOriginal *prox;
 
 } dadosBancariosOriginal;
+
+
+typedef struct dadosOrdenadosNome{
+	struct dadosBancariosOriginal *cliente;
+	struct dadosBancariosOriginal *prox;
+}dadosOrdenadosNome;
 
 
 dadosBancariosOriginal *iniciarlista(){
@@ -16,7 +22,7 @@ dadosBancariosOriginal *iniciarlista(){
 	
 	dado->id = 0;
 	dado->nome[0] = '\0';
-	dado->saldo[0] = '\0';
+	dado->saldo = 0;
 	dado->prox = NULL;
 	return dado;
 }
@@ -31,8 +37,10 @@ void preencherDados(char *minhaString, dadosBancariosOriginal *lista) {
     {
         if (minhaString[i] == '|' || i == strlen(minhaString)){
             for(j; j < i ; j++){
-            	if(minhaString[j] != '|'){
-					
+            	if(minhaString[j] == ',')
+            		aux[auxj] = '.';
+            
+            	if(minhaString[j] != '|' && minhaString[j] != '.'){
             		aux[auxj] = minhaString[j];
             		auxj++;
             		aux[auxj] = '\0';
@@ -45,12 +53,12 @@ void preencherDados(char *minhaString, dadosBancariosOriginal *lista) {
 				
 			if(cont == 1){
 				strcpy(lista->nome , aux);
-				//printf("nome: %s\n",lista->nome);
+			//	printf("nome: %s\n",lista->nome);
 			}
 				
 			if(cont == 2){
-				strcpy(lista->saldo , aux);
-				//printf("saldo: %s\n" ,lista->saldo);
+				lista->saldo = atof(aux);
+				//printf("saldo: %f\n" ,lista->saldo);
 			}
 			auxj = 0;
 			cont++;			
@@ -85,8 +93,7 @@ dadosBancariosOriginal *carregarArquivos( dadosBancariosOriginal *lista){
         			aux = aux->prox;
 				}
 				aux->prox = novo;	
-			}
-            				        
+			}      
         }
 	}
 	return lista;
@@ -97,11 +104,33 @@ void imprimeLista(dadosBancariosOriginal *lista){
 
 	printf("\n\n\n\t\t\t===|LISTA DADOS|===\n\n");
 	while(atual != NULL){
-	   printf("\tid: %i - nome: %s - saldo: %s\n",atual->id, atual->nome, atual->saldo );
+	   printf("\tid: %i - nome: %s - saldo: %f\n",atual->id, atual->nome, atual->saldo );
 	   atual = atual->prox;
 	}
 	printf("\n\n");
 	system("PAUSE");
+}
+
+dadosOrdenadosNome *iniciarlistaNome(){
+	dadosOrdenadosNome *dado = (dadosOrdenadosNome*)malloc(sizeof(dadosOrdenadosNome));
+	
+	dado->cliente = NULL;
+	dado->prox = NULL;
+	return dado;
+}
+
+dadosOrdenadosNome *preencherListaOrdenadaNome(dadosBancariosOriginal *listaOriginal){
+	dadosBancariosOriginal *aux = listaOriginal;
+	dadosOrdenadosNome *listaNome = iniciarlistaNome();
+	
+	listaNome->cliente = listaOriginal;
+	
+	while(aux->prox != NULL){
+		listaNome->prox = aux;
+		aux = aux->prox;		
+	}
+		
+	
 }
 
 int main(){
