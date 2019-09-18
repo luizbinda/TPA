a7ad0e19ae0b1d969ac7d9b7a1a620d62ed7a120
luizbinda <luiznandopimenta52@gmail.com>
2019-09-18 00:00:56 -0300

    att

--- Trabalho Arvore Binaria/Arvore.h
+++ Trabalho Arvore Binaria/Arvore.h
@@ -1,6 +1,116 @@
 typedef struct Nodo {
     int valor;
-    Nodo* esquerda;
-    Nodo* direita;
-    Nodo* pai;
+    struct Nodo* esquerda;
+    struct Nodo* direita;
+    struct Nodo* pai;
 }Nodo;
+
+Nodo* raiz;
+
+Nodo* iniciarArvore();
+Nodo* criarNodo(int valor);
+Nodo* inserirNodo(Nodo* nodo, int novo_valor);
+Nodo* removerNodo(Nodo* nodo, int valor_exluido);
+void percorrerArvoreEmOrdem(Nodo* nodo);
+void percorrerArvorePreOrdem(Nodo* nodo);
+void percorrerArvorePosOrdem(Nodo* nodo);
+Nodo* buscarValor(Nodo* nodo, int valor_procurado);
+
+Nodo* iniciarArvore(){
+    return NULL;
+}
+
+Nodo* criarNodo(int valor){
+    Nodo* novo = (Nodo*)malloc(sizeof(Nodo));
+	novo->valor = valor;
+	novo->direita = NULL;
+	novo->esquerda = NULL;
+	novo->pai = NULL;
+	return novo;
+}
+
+Nodo* inserirNodo(Nodo* nodo, int novo_valor){
+    if(nodo == NULL)
+        nodo = criarNodo(novo_valor);
+    else if ( novo_valor < nodo->valor) {
+        nodo->esquerda = inserirNodo(nodo->esquerda, novo_valor);
+        nodo->esquerda->pai = nodo;
+    }
+    else if ( novo_valor < nodo->valor) {
+        nodo->direita = inserirNodo(nodo->direita, novo_valor);
+        nodo->direita->pai = nodo;
+    }
+    return nodo;
+}
+
+Nodo* removerNodo(Nodo* nodo, int valor_exluido){
+    if(nodo == NULL)
+        return NULL;
+    else if (nodo->valor > valor_exluido) 
+        nodo->esquerda = removerNodo(nodo->esquerda, valor_exluido);
+    else if( nodo->valor < valor_exluido)
+        nodo->direita = removerNodo(nodo->direita, valor_exluido);
+    else {
+        if(nodo->direita == NULL && nodo->esquerda == NULL){
+            free(nodo);
+            nodo = NULL;
+        }            
+        else if(nodo->esquerda == NULL){
+            Nodo* temporario = nodo;
+            nodo->direita->pai = nodo->pai;
+            nodo = nodo->direita;
+            free(temporario);
+        }
+        else if(nodo->esquerda == NULL){
+            Nodo* temporario = nodo;
+            nodo->esquerda->pai = nodo->pai;
+            nodo = nodo->esquerda;
+            free(temporario);
+        }
+        else{
+            Nodo* temporario = nodo->esquerda;
+            while (temporario->direita != NULL )
+                temporario = temporario->direita;
+            
+            nodo->valor = temporario->valor;
+            temporario->valor = valor_exluido;
+
+            nodo->esquerda = removerNodo(nodo->esquerda, valor_exluido);
+        }
+    }   
+}
+
+void percorrerArvoreEmOrdem(Nodo* nodo){
+    if(nodo != NULL){
+        percorrerArvoreEmOrdem(nodo->esquerda);
+        printf("\t-%d-\n", nodo->valor);
+        percorrerArvoreEmOrdem(nodo->direita);    
+    }
+}
+
+void percorrerArvorePreOrdem(Nodo* nodo){
+    if (nodo != NULL){
+        printf("\t-%d-", nodo->valor);
+		percorrerArvorePreOrdem(nodo->esquerda);
+		percorrerArvorePreOrdem(nodo->direita);
+    }
+}
+
+void percorrerArvorePosOrdem(Nodo* nodo){
+    if (nodo != NULL){
+		percorrerArvorePosOrdem(nodo->esquerda);
+		percorrerArvorePosOrdem(nodo->direita);
+        printf("\t-%d-", nodo->valor);
+    } 
+}
+
+Nodo* buscarValor(Nodo* nodo, int valor_procurado){
+    if(nodo == NULL)
+        return NULL;
+    else if(nodo->valor == valor_procurado)
+        return nodo;
+    else if(nodo->valor > valor_procurado)
+        buscarValor(nodo->esquerda, valor_procurado);
+    else
+        buscarValor(nodo->direita, valor_procurado);
+}
--- Trabalho Arvore Binaria/main.c
+++ Trabalho Arvore Binaria/main.c
@@ -2,115 +2,54 @@
 #include <stdio.h>
 #include <string.h>
 #include "DadosBancoOriginal.h"
-#include "Hashing.h"
+#include "Arvore.h"
 
-void menu(Hashing* hash, int tipo_hash){
-	int op = -1;
-    while (op != 0){
-        printf( "1 - Inserir Dados na Hash\n"
-                "2 - Pesquisar Hash\n"
-                "3 - Excluir Hash\n"
-                "0 - Sair\n");
-        scanf("%i",&op);
-		switch (op){
-			case 1:
-				hash = carregarArquivos(hash, 0, tipo_hash);
-				system("PAUSE");
-				system("CLS");
-				break;
-			case 2:{
-				int id_pesquisado;
-				printf("Insira o id que deseja pesquisar: ");
-				scanf("%i", &id_pesquisado);
-				system("CLS");
-				dadosBancarios* aux;
-				if (tipo_hash == 1)						
-					aux = pesquisarHashAberta(hash, id_pesquisado);
-				if (tipo_hash == 3)	
-					aux = pesquisarHashFechada(hash, id_pesquisado);
-				
-				if (aux == NULL)
-					printf("O id pesquisado nao existe...\n");	
-				
-				else if ( aux->excluido == 1)
-					printf("O id pesquisado foi excluido!\n");
-				
-				else
-					printf("\tid: %i - nome: %s - saldo: %.2f\n",aux->id, aux->nome, aux->saldo );
+void menu(){
 
-				system("PAUSE");
-				system("CLS");
-				break;	
-			}
-			case 3:{
-				int id_excluido;
-				printf("Insira o id que deseja pesquisar: ");
-				scanf("%i", &id_excluido);
-				system("CLS");
-				hash = excluirHash(hash, id_excluido, tipo_hash);
-				system("PAUSE");
-				system("CLS");
-				break;
-
-			}
-			case 7:
-				if (tipo_hash == 1)	
-					listarHashAberta(hash);
-				
-				if (tipo_hash == 3)		
-					listarHashFechada(hash);				
-
-				printf("total colisao: %i\n", listarColisao(hash));
-				printf("tamanho hash: %i\n", hash->tamanho_hash);
-				printf("total hash: %i\n", hash->total);
-				system("PAUSE");
-				system("CLS");
-				break;
-			
-			case 0:
-				if (tipo_hash == 1)
-					gerarAquivoHash(hash, "HashingAberta.csv");
-				if (tipo_hash == 2)
-					gerarAquivoHash(hash, "HashingAbertaDobra.csv");
-				if (tipo_hash == 3)
-					gerarAquivoHash(hash, "HashingFechada.csv");
-				
-				printf("Finalizado...\n");
-				break;	
-			default:
-				printf("Opcao invalida!!\n");
-				system("PAUSE");
-				system("CLS");
-				break;
-		}
-	}
 }
 
 
 int main(){
-	
-	int tipo_hash = -1;
-    while (tipo_hash != 0){
-		printf( "1 - Hash Aberta por divisao\n"
-				"2 - Hash Aberta por Dobra\n"
-				"3 - Hash Fechada\n"
-				"0 - Sair\n");
-		scanf("%i",&tipo_hash);
-		system("CLS");
+   	raiz = iniciarArvore(); 
+	int op = -1;
+	int valor;
+    while (op != 0){
+        printf( "1 - Inserir novo elemento\n"
+                "2 - Exluir elemento\n"
+                "3 - Buscar elemento\n"
+                "4 - Imprimir em ordem\n"
+                "0 - Sair\n");
+        scanf("%i",&op);
+		switch (op){
+			case 1:{
+				printf("Digite o numero que deseja inserir: ");
+				scanf("%i", &valor);
+				raiz = inserirNodo(&raiz, valor);
+				system("pause");
+				system("cls");
+				break;
+			}
+			case 2:{
+				printf("Digite o numero que deseja inserir: ");
+				scanf("%d", &valor);
+				removerNodo(raiz, valor);
+				break;	
+			}
+			case 3:{
+				printf("Digite o numero que deseja inserir: ");
+				scanf("%d", &valor);
+				buscarValor(raiz, valor);
+				break;
 
-		if (tipo_hash < 1 || tipo_hash > 3)
-			printf("Opcao invalida!\n");
-		else{
-			Hashing* hash = inciarHash(500);
-			hash = inciarVetor(hash);
-			colisao = inciarVetorColisao(colisao, hash->tamanho_hash);
-			hash = carregarArquivos(hash, 1, tipo_hash);
-			system("CLS");
-			menu(hash, tipo_hash);
-			system("CLS");
-		}				
-		system("CLS");	
-		
+			}
+			case 4:{
+				percorrerArvoreEmOrdem(raiz);
+				system("pause");
+				system("cls");
+
+				break;
+			}
+		}
 	}
 	return 0;
 }
