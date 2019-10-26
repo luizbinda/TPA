21127f45c3a590f3171cce1f31cae25cb246c3cd
Luiz Fernando Binda Pimenta <luiznandopimenta52@gmail.com>
2019-10-26 14:41:53 -0300

    struct grafoBairros

---
+++ TrabalhoGrafosArvoreGeradoraMinima/structs.h
@@ -0,0 +1,13 @@
+
+typedef struct grafoBairros{
+	
+	char **bairros;				//vetor de string com nome das bairros
+	float **distancias;			//matriz de distancias das cidades
+	int numero_vertices;		//quantidades de linhas e colunas da matriz
+	
+}grafoBairros;
+
+
+
+
+
